3b2ac2d6c6b433a7dd066fa27744b9b94ef434b1
boyarifsetiawan <boysetiawan5635@gmail.com>
2024-01-03 23:05:02 +0700

    show modal edit

--- resources/js/components/pages/users/ListUsers.vue
+++ resources/js/components/pages/users/ListUsers.vue
@@ -1,29 +1,53 @@
 <script setup>
     import { onMounted, reactive, ref } from "vue";
+    import { Form, Field } from "vee-validate";
+    import * as yup from "yup";
     import axios from 'axios';
+    
     const users = ref([]);
-    const form = reactive({
-        name: '',
-        email: '',
-        password: ''
-    })
+    const editing = ref(false)
+    const formValues = ref();
+    const form = ref(null)
+    
     const getUsers = () => {
         axios.get('/api/users')
-        .then((res) => {
-            users.value = res.data.data
-            console.log(res.data.data)
+        .then((response) => {
+            users.value = response.data.data
         })
     }
 
-    const createUser = () => {
-        axios.post('/api/users', form)
-        .then((res) => {
-            users.value.unshift(res.data.data)
-            form.name = '',
-            form.email = '',
-            form.password = '',
-            $('#createUserModal').modal('hide');
+    //validation-schema
+    const schema = yup.object({
+        name: yup.string().required(),
+        email: yup.string().email().required(),
+        password: yup.string().required().min(8)
+    });
+
+    //show modal for create
+    const addUser = () => {
+        editing.value = false
+        $('#userFormModal').modal('show')
+    }
+    //create user
+    const createUser = (values, { resetForm }) => {
+        axios.post('/api/users', values)
+                    .then((response) => {
+                        users.value.unshift(response.data.data)
+                        $('#userFormModal').modal('hide');
+                        resetForm()
         })
+    }
+
+    //edit User modal
+    const editUser = (user) => {
+        editing.value = true
+        form.value.resetForm();
+        $('#userFormModal').modal('show');
+        formValues.value = { 
+            id: user.id,
+            name: user.name,
+            email: user.email,
+        };
     }
 
     onMounted(() => {
@@ -48,7 +72,7 @@
             </div>
         </div>
         <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#createUserModal">
+        <button type="button" class="btn btn-primary" @click="addUser">
             Create User
         </button>
     </div>
@@ -72,7 +96,9 @@
                         <td>{{ user.email }}</td>
                         <td>-</td>
                         <td>-</td>
-                        <td>-</td>
+                        <td>
+                            <a href="#" @click.prevent="editUser(user)"><i class="fa fa-edit"></i></a>
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -81,41 +107,46 @@
 
 
     <!-- Modal -->
-    <div class="modal fade" id="createUserModal" data-backdrop="static" data-keyboard="false" tabindex="-1"
+    <div class="modal fade" id="userFormModal" data-backdrop="static" data-keyboard="false" tabindex="-1"
         aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Create User :</h5>
+                    <h5 class="modal-title" id="staticBackdropLabel">
+                        <span v-if="editing">Edit User</span>
+                        <span v-else>Create User</span>
+                    </h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <div class="modal-body">
-                        <div class="form-group">
-                            <label for="name">Name</label>
-                            <input v-model="form.name" name="name" type="text" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Enter full name" />
-                            
-                        </div>
+                <Form ref="form" @submit="createUser" :validation-schema="schema" v-slot="{errors}" :initial-values="formValues">
+                    <div class="modal-body">
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <Field name="name" v-model="formValues.name" type="text" class="form-control" :class="{'is-invalid': errors.name}" id="name" aria-describedby="nameHelp" placeholder="Enter full name" />
+                                <span class="invalid-feedback">{{ errors.name }}</span>
+                            </div>
 
-                        <div class="form-group">
-                            <label for="email">Email</label>
-                            <input v-model="form.email" name="email" type="email" class="form-control "
-                                 id="email" aria-describedby="nameHelp" placeholder="Enter full name" />
-                           
-                        </div>
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <Field name="email" type="email" class="form-control" :class="{'is-invalid': errors.email}"
+                                    id="email" aria-describedby="nameHelp" placeholder="Enter an Email" />
+                                <span class="invalid-feedback">{{ errors.email }}</span>
+                            </div>
 
-                        <div class="form-group">
-                            <label for="email">Password</label>
-                            <input v-model="form.password" name="password" type="password" class="form-control " id="password" aria-describedby="nameHelp"
-                                placeholder="Enter password" />
-                            
+                            <div class="form-group">
+                                <label for="email">Password</label>
+                                <Field name="password" type="password" class="form-control" :class="{'is-invalid': errors.password}" id="password" aria-describedby="nameHelp"
+                                    placeholder="Enter password" />
+                                <span class="invalid-feedback">{{ errors.password }}</span>
+                            </div>
                         </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Add</button>
                     </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button @click="createUser" type="button" class="btn btn-primary">Add</button>
-                </div>
+                </Form>
             </div>
         </div>
     </div>
--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Starter</title>
+    <title>laravel Multipurpose</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
